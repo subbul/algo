2807d72f687ad5c989c57f02f89def1ba41a5be2
Subramanian L <sublaksh@cisco.com>
2014-09-24 09:29:36 +0530

    Initial commit

---
+++ insertion_sort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Insertion sort
+* 1. Given array is split into  
+* 			a.) first element on left half (start index be i ) 
+*			b.) remaining elements on right half (start index be j)
+* 2. Compare first element in right half with each element on left half starting j-1 till i
+*			a.) if j-1 is greater than j, swap j-1 with j 
+*			b.) continue comparing now j-1 with j-2 and so on till i
+*			c.)
+* 3. start from last j+1 and repeat #2
+*
+*
+*/
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
